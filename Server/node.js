2130646bc45672eb3834cdaa8722e61befe869a7
Sina Mirshafiei <sinamirshafiee@me.com>
2023-11-09 16:29:52 +0100

    Adding dummy chart

--- Server/node.js
+++ Server/node.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 
 app.use(express.static("."));
+app.use(express.static("../"));
+app.use(express.static("File"));
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
