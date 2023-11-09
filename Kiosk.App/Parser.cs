9101659258d02b9446c871a700feee5ba90b757b
Islam <k.h.r.islam@tue.nl>
2023-11-09 16:18:24 +0100

    created some dummy questions

--- Kiosk.App/Parser.cs
+++ Kiosk.App/Parser.cs
@@ -9,16 +9,19 @@ class Parser{
 
         Question<String> firstQuestion = new Question<string>();
         firstQuestion.QuestionDescription = "Are you a software Eingineer? ";
+        firstQuestion.SetQuestionType(QuestionType.SingleAnswer);
         var questionOneOptions = new List<string> {"Yes", "No"};
-        //firstQuestion.SetPossibleAnswers(questionOneOptions);
+        firstQuestion.SetPossibleAnswers(questionOneOptions);
         questionList.Add(firstQuestion);
 
         Question<String> secondQuestion = new Question<string>();
         secondQuestion.QuestionDescription = "What is your Age?";
+        secondQuestion.SetQuestionType(QuestionType.Descriptional);
         questionList.Add(secondQuestion);
 
         Question<String> thirdQuestion = new Question<string>();
         thirdQuestion.QuestionDescription = "What is your favourite food?";
+        thirdQuestion.SetQuestionType(QuestionType.SingleAnswer);
         var questionThreeOptions = new List<string> {"Rice", "Burger"};
         //thirdQuestion.SetPossibleAnswers(questionThreeOptions);
         questionList.Add(thirdQuestion);
