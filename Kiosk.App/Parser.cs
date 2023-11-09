e4feac1c2ca62f289abea2460e14a118a17d69d8
Islam <k.h.r.islam@tue.nl>
2023-11-09 16:06:20 +0100

    Added some dummy data

--- Kiosk.App/Parser.cs
+++ Kiosk.App/Parser.cs
@@ -7,12 +7,25 @@ class Parser{
 
         List< Question<String>> questionList = new List< Question<String>>();
 
+        Question<String> firstQuestion = new Question<string>();
+        firstQuestion.QuestionDescription = "Are you a software Eingineer? ";
+        var questionOneOptions = new List<string> {"Yes", "No"};
+        //firstQuestion.SetPossibleAnswers(questionOneOptions);
+        questionList.Add(firstQuestion);
 
-        Question<String> question = new Question<string>();
-        question.QuestionDescription = "Are you a software Eingineer? ";
+        Question<String> secondQuestion = new Question<string>();
+        secondQuestion.QuestionDescription = "What is your Age?";
+        questionList.Add(secondQuestion);
+
+        Question<String> thirdQuestion = new Question<string>();
+        thirdQuestion.QuestionDescription = "What is your favourite food?";
+        var questionThreeOptions = new List<string> {"Rice", "Burger"};
+        //thirdQuestion.SetPossibleAnswers(questionThreeOptions);
+        questionList.Add(thirdQuestion);
+
+       
 
 
-        //ljonkjbkhbhbjhb
         return questionList;
 
     }
