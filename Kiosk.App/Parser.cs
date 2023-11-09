18fb8829bac28f4b2aa7d21e995eeb6bf8b92036
Islam <k.h.r.islam@tue.nl>
2023-11-09 15:43:39 +0100

    add a class to pass data

---
+++ Kiosk.App/Parser.cs
@@ -0,0 +1,19 @@
+using Kiosk.App;
+
+class Parser{
+
+
+    public List<Question<String>> getAllTheQuestion(){
+
+        List< Question<String>> questionList = new List< Question<String>>();
+
+
+        Question<String> question = new Question<string>();
+        question.QuestionDescription = "Are you a software Eingineer? ";
+
+
+        //ljonkjbkhbhbjhb
+        return questionList;
+
+    }
+}
